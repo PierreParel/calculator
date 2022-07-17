5297dda64de43695292114c3d3fd6314384e2849
Pierre Parel <pirparel03@gmail.com>
2022-07-17 14:17:54 +0800

    Add variables, functions, and event listeners.
    updateDisplay function was created to update the display whenever a
    button was pressed. Event listeners were also added to all buttons.
    Functionality has been implemented to the number buttons, the
    operator buttons, and the clear button.

--- script.js
+++ script.js
@@ -1,7 +1,82 @@
 
-let num1, num2, ans;
+let num1 = null, 
+    num2 = null, 
+    op1 = null,
+    op2 = null,
+    displayValue = '0';
+    
 let operation;
+const buttons = document.querySelectorAll("button");
 
+function updateDisplay(){
+    const display = document.getElementById("display");
+    display.innerText = displayValue;
+}
+
+updateDisplay();
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(button.classList.contains('number')){
+            let num = e.target.value;
+            if(displayValue == 0 || displayValue == num1){
+                displayValue = num;
+            }else{
+                displayValue+= num;
+            }
+        }else if(button.classList.contains('operator')){
+            let butt = e.target.value;
+            if(op1 == null){
+                op1 = butt;
+                console.log(op1);
+                num1 = displayValue;
+            }else if(op1 != null){
+                op2 = butt;
+                num2 = displayValue;
+                inputNumbers();
+            }
+        }else if(button.classList.contains('clear')){
+            resetCalc();
+        }
+        updateDisplay();
+    });
+});
+
+
+function inputNumbers(){
+    console.log(`num1:${num1}\nnum2:${num2}\nop1:${op1}\nop2:${op2}`)
+
+    if(num1 == null){
+        num1 = displayValue;
+    }else if(num1 != null){
+        num1 = operate(num1,num2);
+        console.log(`num1: ${num1}`);
+        op1 = op2;
+        op2 = null;
+        displayValue = num1;
+        num2 = null;
+    }
+}
+
+
+function inputDecimal(){
+}
+
+function inputPercent(){
+    displayValue = displayValue/100;
+}
+
+function resetCalc(){
+    num1 = null, 
+    num2 = null, 
+    op1 = null, 
+    op2 = null, 
+    displayValue = '0';
+}
+
+function changeSign(){
+
+}
 
 function add(x,y){
     return x+y;
@@ -19,22 +94,30 @@ function divide(x,y){
     return x/y;
 }
 
-function operate(op,x,y){
+function operate(x,y){
     let ans = 0;
-    switch (op){
-        case "add":
+    x = Number(x);
+    y = Number(y);
+    console.log(`\n\n OPERATOR: ${op1}`);
+    switch(op1){
+        case "+":
             ans = add(x,y);
             break;
-        case "subtract":
+        case "-":
             ans = subtract(x,y);
             break;
-        case "multiply":
+        case "*":
             ans = multiply(x,y);
             break;
-        case "divide":
-            ans = divide(x,y);
+        case "/":
+            if(y == 0){
+                displayValue = "YOU CANNOT DIVIDE BY 0 BOZO"
+            }
+            else{
+                ans = divide(x,y);
+            }
+            
             break;
     }
-
     return ans;
 }
